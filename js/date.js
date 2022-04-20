ff17fffc3cb9e63046346e03410c93e5e61aef2a
Jen019 <ree17005@byui.edu>
2022-04-20 14:29:07 -0600

    Added JavaScript code and made changes

---
+++ js/date.js
@@ -0,0 +1,8 @@
+
+var date = new Date();
+var year = date.getFullYear();
+document.querySelector("span").textContent = year;
+
+
+const UpdateDate = document.lastModified;
+document.getElementById("lastUpdateDate").textContent = UpdateDate;
