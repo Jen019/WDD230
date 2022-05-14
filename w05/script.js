395641f56309661882c5a20fc08f5ec9b764953f
Jen019 <ree17005@byui.edu>
2022-05-14 15:08:16 -0600

    week 5 activity

---
+++ w05/script.js
@@ -0,0 +1,28 @@
+const list = document.querySelector('ul');
+const input = document.getElementById('favChap');
+const button = document.getElementById('addChapter');
+
+button.addEventListener('click', function() {
+    const favoriteVerse = input.value;
+    if (favoriteVerse.length == 0)
+      { 
+         alert("Input is Blank Please Enter a Favorite Chapter");  	
+         return false; 
+      }  	
+
+    const listScripture = document.createElement('li');
+    const listText = document.createElement('span');
+    const deleteBtn = document.createElement('button');
+
+    listScripture.appendChild(listText);
+    listText.textContent = favoriteVerse;
+    listScripture.appendChild(deleteBtn);
+    deleteBtn.textContent = 'Delete';
+    list.appendChild(listScripture);
+
+    deleteBtn.addEventListener('click', function() {
+      list.removeChild(listScripture);
+    });
+
+    input.focus();
+  });
