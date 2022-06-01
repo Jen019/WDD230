9269d0289ba5b74d0425f54336be12c52a841c71
Jen019 <ree17005@byui.edu>
2022-06-01 14:06:09 -0600

    finishing touches

---
+++ chamber/discover/js/localStorage.js
@@ -0,0 +1,18 @@
+let lastVisitDate;
+    const now = Date.now();
+    if(window.localStorage.getItem('last-visit-date')){
+        lastVisitDate = window.localStorage.getItem('last-visit-date');
+    
+    }
+      else{
+          lastVisitDate = now;
+      }
+
+      window.localStorage.setItem('last-visit-date', now);
+
+      const secondsSinceLastVisit = Math.floor((now - lastVisitDate) / 1000);
+      const daysSinceLastVisit = Math.floor(secondsSinceLastVisit / 86400); //dividing by seconds in a day
+
+      document.querySelector('#seconds-ago').textContent = secondsSinceLastVisit;
+      document.querySelector('#days-ago').textContent= daysSinceLastVisit;
+      
