cc5a630852a5e5ca5851690b65e1fcc19d0b3fa6
Jen019 <ree17005@byui.edu>
2022-06-15 11:43:09 -0600

    fix buttons

--- chamber/directory/js/toggleDisplay.js
+++ chamber/directory/js/toggleDisplay.js
@@ -1,18 +1,19 @@
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
-const display = document.querySelector("div .cards");
+const display = document.querySelector(".cards");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
-
-gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("cards");
-	display.classList.remove("list");
-});
 
 listbutton.addEventListener("click", showList); // example using defined function
 
 function showList() {
 	display.classList.add("list");
 	display.classList.remove("cards");
+}
+
+gridbutton.addEventListener("click",showGrid);
+
+function showGrid(){
+	display.classList.add("cards");
+	display.classList.remove("list");
 }
