f1dbd0d0ab285f44ee802a34f254a71fd17373f8
Jen019 <ree17005@byui.edu>
2022-06-15 16:41:15 -0600

    major display changes

--- chamber/directory/js/business.js
+++ chamber/directory/js/business.js
@@ -14,7 +14,7 @@ const businesses = jsonObject['businesses'];
 
 function displayBusinesses(businesses) {
    
-    let card = document.createElement('section');
+    let card = document.createElement('div');
     let h2 = document.createElement('h2');
     let p1 = document.createElement('p');
     let p2 = document.createElement('p');
@@ -24,12 +24,20 @@ function displayBusinesses(businesses) {
 
     logo.className = 'businessImg';
     card.className = 'businessData';
+    p1.className = 'card-desc';
 
-    h2.textContent = `${businesses["name"]}`;
-    p1.textContent = `${businesses["address"]}`;
-    p2.textContent = `${businesses["phonenumber"]}`;
-    p3.textContent = `${businesses["website"]}`
-    p4.textContent = `${businesses["description"]}`;
+    h2.textContent = `${businesses["name"]}`
+    p1.textContent = `Address: ${businesses["address"]}
+    
+    
+    Phonenumber: ${businesses["phonenumber"]}
+    
+   
+    
+    Website: ${businesses["website"]}
+    
+  
+    Description: ${businesses["description"]}`;
   
     
     logo.setAttribute('src', `${businesses["imageURL"]}`);
@@ -39,9 +47,8 @@ function displayBusinesses(businesses) {
     card.appendChild(h2);
     card.appendChild(logo);
     card.appendChild(p1);
-    card.appendChild(p2);
-    card.appendChild(p3);
-    card.appendChild(p4);
+   
   
-    document.querySelector('div.cards').appendChild(card);
+    document.querySelector('div.cards section').appendChild(card);
+
   }
