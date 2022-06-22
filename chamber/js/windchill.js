eaecc2f17a26ee79c3b6e6cfc1a89bd788253a91
Jen019 <ree17005@byui.edu>
2022-06-22 10:12:37 -0600

    console log

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,6 +1,9 @@
 const temp = document.getElementById("temp").textContent;
 const windSpeed = document.getElementById("speed").textContent;
 
+console.log(temp);
+console.log(windSpeed);
+
 if(temp <=50 && windSpeed >= 3.0){
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed,0.16) + 0.4275 * temp * Math.pow(windSpeed,0.16);
 
