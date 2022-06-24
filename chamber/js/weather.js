352c9abbd69bcb53b62e9e84bcf6cce4e517b793
Jen019 <ree17005@byui.edu>
2022-06-24 11:36:03 -0600

    fixes

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -2,24 +2,38 @@ const currentTemp = document.querySelector('#temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const wind = document.querySelector('#speed');
 const captionDesc = document.querySelector('figcaption');
-const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Idaho Falls,us&units=imperial&appid=3152f5cb63193c20d2712d8adecda329";
+const base_url = "https://api.openweathermap.org/data/2.5/weather";
+const city_name = "Idaho Falls";
+const state_code = "ID";
+const country_code = "US";
+const appid = "3152f5cb63193c20d2712d8adecda329";
+const units = "imperial";
+const weatherUrl = `${base_url}?q=${city_name},${state_code},${country_code}&appid=${appid}&units=${units}`;
 
-fetch(weatherURL)
+console.log("weather.json");
+
+fetch(weatherUrl)
   .then((response) => response.json())
   .then((data) => {
     console.log(data); // this is temporary for development only
-    currentTemp.textContent = `${data.main.temp.toFixed(0)}`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    const desc = data.weather[0].description;
+    const temp = data.main.temp;
+    const windSpeed = data.wind.speed;
     const speed = data.wind.speed;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const desc = data.weather[0].description;
+
+    currentTemp.textContent = Math.round(temp);
+    wind.textContent = speed;
+   
   
-  weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
+  
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
   captionDesc.textContent = desc;
-  wind.textContent = speed;
 
 
-if(currentTemp <=50 && wind >= 3.0){
-    let wc = 35.74 + 0.6215 * currentTemp - 35.75 * Math.pow(wind,0.16) + 0.4275 * currentTemp * Math.pow(wind,0.16);
+if(temp <=50 && windSpeed >= 3.0){
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed,0.16) + 0.4275 * temp * Math.pow(windSpeed,0.16);
 
     wc = Math.round(wc);
 
