87c7a1e1ae261d025a5245f4fa88bc2da9f34439
Jen019 <ree17005@byui.edu>
2022-06-24 12:16:19 -0600

    fixes

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -5,3 +5,9 @@ function toggleMenu(){
 
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
+
+const date = new Date();
+const year = date.getFullYear();
+
+document.getElementById("currentYear").textContent = year;
+document.getElementById("lastUpdateDate").textContent = document.lastModified;
