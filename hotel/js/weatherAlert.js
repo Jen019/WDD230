848d743a331ee8f5540d588b3b03079a2f47c650
Jen019 <ree17005@byui.edu>
2022-07-06 14:23:47 -0600

    fixes

--- hotel/js/weatherAlert.js
+++ hotel/js/weatherAlert.js
@@ -3,7 +3,7 @@ const wEvent = document.querySelector('#event');
 const start = document.querySelector('#start');
 const end = document.querySelector('#end');
 const wDesc = document.querySelector('#description');
-const tags = document.querySelector('#tag');
+const alertBanner = document.querySelector('#weatherAlert');
 
 
 const weatherAlertURL = "https://api.openweathermap.org/data/2.5/onecall?lat=43.6121&lon=-116.3915&excludes=hourly,minutely&units=imperial&appid=3152f5cb63193c20d2712d8adecda329";
@@ -12,7 +12,6 @@ fetch("js/alerts.json")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
- 
     const senderName = data.alerts[0].sender_name;
     const weatherEvent = data.alerts[0].event;
     const startDate = data.alerts[0].start;
@@ -32,4 +31,5 @@ fetch("js/alerts.json")
     end.textContent = ed;
     wDesc.textContent = weatherDesc;
     //tags.textContent = alertTag;
+    
   });
