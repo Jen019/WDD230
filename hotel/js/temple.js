f6387d45efbc623977053c8b7e1ac41e15bfcb86
Jen019 <ree17005@byui.edu>
2022-07-08 13:09:31 -0600

    changes to temple

--- hotel/js/temple.js
+++ hotel/js/temple.js
@@ -11,42 +11,24 @@ const temples = jsonObject['temples'];
 });
 
 function displayTemples(temples) {
-    let cb = document.createElement('div');
     let h2 = document.createElement('h2');
     let ul = document.createElement('ul');
     let p1 = document.createElement('li');
-    let p2 = document.createElement('li');
-    let p3 = document.createElement('li');
-    let p4 = document.createElement('li');
-    let p5 = document.createElement('li');
     let templeImg = document.createElement('img');
 
     ul.className = 'temp-desc';
-    document.querySelector('.front').appendChild(h2);
     templeImg.className = 'tempImg';
     h2.textContent = `${temples["templeName"]}`;
     p1.textContent = 
-    `Address: ${temples["streetAddress"]}`;
-    p2.textContent = `${temples["cityAddress"]}`;
-    p3.textContent = `${temples["stateAddress"]}`;
-    p4.textContent = `${temples["zipcode"]}`;
-    p5.textContent = `${temples["country"]}`;
+    `Address: ${temples["address"]}`;
 
-    document.querySelector('.back').appendChild(ul);
-
-    ul.appendChild(p1);
-    ul.appendChild(p2);
-    ul.appendChild(p3);
-    ul.appendChild(p4);
-    ul.appendChild(p5);
-  
-    
+   
     templeImg.setAttribute('src', `${temples["templeImg"]}`);
     templeImg.setAttribute('alt', `${temples["describe"]}`);
     templeImg.setAttribute('loading', 'lazy');
-
+    document.querySelector('.front').appendChild(h2);
     document.querySelector('.front').appendChild(templeImg);
-   
-    document.querySelector('div.back').appendChild(cb);
+    document.querySelector('.front').appendChild(ul); 
+    ul.appendChild(p1);
 
   }
