bd5440ac6983bf5754ab2c340317dea5ec383a5b
Jen019 <ree17005@byui.edu>
2022-07-13 19:00:50 -0600

    book now button

--- hotel/js/toggleButton.js
+++ hotel/js/toggleButton.js
@@ -6,6 +6,10 @@ function toggleMenu(){
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
 
+document.getElementById("message").onclick = function (){
+    location.href ="https://jen019.github.io/wdd230/hotel/reservation.html";
+};
+
 
 
 
