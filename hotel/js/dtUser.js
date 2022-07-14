99f55db60c902ce466483d277c68be2df8befbe1
Jen019 <ree17005@byui.edu>
2022-07-14 12:18:41 -0600

    final changes

---
+++ hotel/js/dtUser.js
@@ -0,0 +1,8 @@
+const d = new Date();
+
+document.getElementById("date").value = d.toDateString();
+
+const hours = d.getHours();
+const mins = d.getMinutes();
+const seconds = d.getSeconds();
+document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
